193a9fc16f260b13517d0e0d359beb72c473867b
mdmmalamin <mdmmalamin@gmail.com>
2024-03-28 18:42:30 +0600

    fixed some issues

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,9 @@
 import Main from "@/Layout/Main";
 import Home from "@/Pages/Home/Home";
 import AboutUs from "@/Pages/AboutUs/AboutUs";
-import Blog from "@/Pages/Blog/Blog";
 import ContactUs from "@/Pages/ContactUs/ContactUs";
 import { createBrowserRouter } from "react-router-dom";
-import DisplayError from '@/components/DisplayError';
+import DisplayError from "@/components/DisplayError";
 import ShipChandlery from "@/Pages/Services/ShipChandlery";
 import ShipRepair from "@/Pages/Services/ShipRepair";
 
@@ -12,40 +11,40 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
-    errorElement: <DisplayError/>,
+    errorElement: <DisplayError />,
     children: [
       {
         path: "/",
         element: <Home />,
       },
       {
-        path: '/about-us',
-        element: <AboutUs />
+        path: "/about-us",
+        element: <AboutUs />,
       },
-   
+      // Services Routes ⤵
       {
-        path: '/service/ship-supply',
-        element: <Service />
-      },
-      {
-        path: '/service/logistics-services',
-        element: <Service />
+        path: "/service/ship-supply",
+        // element: <Service />,
       },
       {
-        path: '/service/export-products-services',
-        element: <Service />
+        path: "/service/logistics-services",
+        // element: <Service />,
+      },
+      {
+        path: "/service/export-products-services",
+        // element: <Service />,
+      },
+      {
+        path: "/service/ship-chandlery",
+        element: <ShipChandlery />,
+      },
+      {
+        path: "/service/ship-repair",
+        element: <ShipRepair />,
       },
       {
         path: "/contact-us",
         element: <ContactUs />,
-      },
-      {
-        path: "/shipChandlery",
-        element: <ShipChandlery/>,
-      },
-      {
-        path: "/shipRepair",
-        element: <ShipRepair/>,
       },
     ],
   },
