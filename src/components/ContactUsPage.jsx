2263a2fa586411c4f0c8c9843a3681580e0b5905
naimur2001 <naimurrahman18thapril@gmail.com>
2024-05-15 23:39:20 +0600

    conatct us page modal done

--- src/components/ContactUsPage.jsx
+++ src/components/ContactUsPage.jsx
@@ -1,10 +1,12 @@
 import "@/components/Style/Umeew.css";
+import { useState } from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 const ContactUsPage = () => {
   window.scrollTo({
     top: 0,
     behavior: "smooth",
   });
+  const [openModal, setOpenModal] = useState(false);
 
   return (
     <div className="main-div">
@@ -16,12 +18,47 @@ const ContactUsPage = () => {
 
       <div className="part-1-2 my-10 ">
         <div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-10">
+        <div
+  className={`fixed inset-0 flex items-center justify-center z-50 ${
+    openModal ? 'scale-1 opacity-1 duration-300' : 'scale-0 opacity-0 duration-150'
+  }`}
+  onClick={() => setOpenModal(false)}
+>
+
+</div>
+
           {/* part one div form section basically */}
           <div className="part1 basis-1/2 flex flex-col items-center  p-8 rounded-xl shadow-md">
             <h1 className="typewriter-anim   lg:text-4xl md:text-3xl text-2xl font-semibold my-10 text-center">
               Leave A Message
             </h1>
             <form action="" method="POST" className="w-full max-w-lg ">
+                {/* modal */}
+  <div
+  className={`fixed inset-0 flex items-center justify-center z-50 ${
+    openModal ? 'scale-100 opacity-100 duration-300' : 'scale-0 opacity-0 duration-150'
+  }`}
+  onClick={() => setOpenModal(false)}
+>
+  <div
+    onClick={(e_) => e_.stopPropagation()}
+    className="max-w-md rounded-lg bg-slate-50 p-6 drop-shadow-lg dark:bg-gray-800 dark:text-white"
+  >
+    <h1 className="mb-2 text-2xl font-semibold">Thank You!</h1>
+    <p className="mb-5 text-sm opacity-80">We appreciate your support and hope you enjoy staying With BackbencherItHouse .</p>
+    <div className="flex justify-center">
+  
+      <button
+        onClick={() => setOpenModal(false)}
+        className="rounded-md border border-rose-600 px-6 py-[6px] text-rose-600 duration-150 hover:bg-rose-600 hover:text-white"
+      >
+        Close
+      </button>
+    </div>
+  </div>
+</div>
+
+  {/* modal */}
               <div className="flex flex-wrap  -mx-3 mb-6">
                 <div className="w-full md:w-1/2  px-3 mb-6 md:mb-0">
                   <input
@@ -77,12 +114,28 @@ const ContactUsPage = () => {
                   />
                 </div>
               </div>
+              
               <button
                 type="submit"
                 className={`w-full px-6 md:px-8 lg:px-8 py-2 md:py-3 lg:py-3 bg-secondary rounded font-semibold text-white outline outline-1 capitalize transition ease duration-300 hover:translate-x-1 hover:-translate-y-1 `}
+
+                onClick={() => setOpenModal(true)}
               >
                 Contact Us
               </button>
+
+                        <div onClick={(e_) => e_.stopPropagation()} className={`text- absolute max-w-md rounded-lg bg-white p-6 drop-shadow-lg dark:bg-gray-800 dark:text-white ${openModal ? 'scale-1 opacity-1 duration-300' : 'scale-0 opacity-0 duration-150'}`}>
+            <h1 className="mb-2 text-2xl font-semibold">Welcome to NavigateUI!</h1>
+            <p className="mb-5 text-sm opacity-80">Elevate your React projects with beautifully crafted components designed for TailwindCSS.</p>
+            <div className="flex justify-between">
+              <button onClick={() => setOpenModal(false)} className="me-2 rounded-md bg-indigo-600 hover:bg-indigo-700 px-6 py-[6px] text-white">
+                Ok
+              </button>
+              <button onClick={() => setOpenModal(false)} className="rounded-md border border-rose-600 px-6 py-[6px] text-rose-600 duration-150 hover:bg-rose-600 hover:text-white">
+                Cancel
+              </button>
+            </div>
+          </div>
             </form>
           </div>
 
